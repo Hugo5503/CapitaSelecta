27e4f973a6b3caac9ff6462dc4f71d4cb588db18
Themagman <hugoleenen@hotmail.com>
2018-01-10 17:29:14 +0100

    Start up able to manually control boat simple island layout

---
+++ Capita Selecta/Assets/Scripts/ShipControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControl : MonoBehaviour
+{
+
+    public bool forward;
+    public float speed;
+    public bool rotate;
+    public float turningSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (forward)
+        {
+            this.GetComponent<Rigidbody>().velocity = transform.forward * (speed * Time.deltaTime);
+        }
+        if (rotate)
+        {
+            this.transform.Rotate(Vector3.up * Time.deltaTime * turningSpeed);
+        }
+    }
+}
